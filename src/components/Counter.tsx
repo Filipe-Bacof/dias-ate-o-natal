056e25fd54c719d97479d3edc794f060b6e18648
Filipe Bacof <filipebacof@gmail.com>
2025-03-17 23:11:18 -0300

    feat: imagine que hoje é o natal

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,6 @@
 import { Timer } from "@/types/timer";
 import { getTextColorClass } from "@/utils/colors";
+import Link from "next/link";
 
 type CounterProps = {
     timeLeft: Timer;
@@ -8,17 +9,25 @@ type CounterProps = {
 export default function Counter ({timeLeft}: CounterProps) {
     const color = getTextColorClass(timeLeft.days);
     return (
-        <div
-            className={
-                `${color} flex flex-col items-center justify-center
-                w-full h-screen bg-black text-2xl font-bold text-center`
-            }
-        >
-            <p>Faltam</p>
-            <p>
-            {timeLeft.days} dias, {timeLeft.hours} horas, {timeLeft.minutes} minutos e {timeLeft.seconds} segundos
-            </p>
-            <p>para o Natal!!!</p>
-        </div>
+        <>
+            <div
+                className={
+                    `${color} flex flex-col items-center justify-center
+                    w-full h-screen bg-black text-2xl font-bold text-center`
+                }
+            >
+                <p>Faltam</p>
+                <p>
+                {timeLeft.days} dias, {timeLeft.hours} horas, {timeLeft.minutes} minutos e {timeLeft.seconds} segundos
+                </p>
+                <p>para o Natal!!!</p>
+            </div>
+            <Link
+                href="/natal"
+                className="fixed bottom-0 text-white underline cursor-pointer"
+            >
+                Imagine que hoje é o natal 🎄
+            </Link>
+        </>
     )
 }
