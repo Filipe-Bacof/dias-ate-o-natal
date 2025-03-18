791e3d034dce951036ef93471f9baac857052535
Filipe Bacof <filipebacof@gmail.com>
2025-03-17 23:06:12 -0300

    feat: autoplay no celular mutado

--- src/components/RobertoCarlos.tsx
+++ src/components/RobertoCarlos.tsx
@@ -1,13 +1,20 @@
-import { isMobile } from "@/utils/mobile";
+'use client';
+import { useEffect, useState } from "react";
 import { urlRobertoCarlos, urlRobertoCarlosMutada } from "@/utils/url";
 
 export default function RobertoCarlos() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth <= 768);
+    }, []);
+
     return (
         <iframe
             className="w-full h-full"
             width="560"
             height="315"
-            src={isMobile() ? urlRobertoCarlosMutada : urlRobertoCarlos}
+            src={isMobile ? urlRobertoCarlosMutada : urlRobertoCarlos}
             title="Feliz Natal"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/utils/mobile.ts
+++
@@ -1,5 +0,0 @@
-function isMobile () {
-    return typeof window !== "undefined" && window.innerWidth <= 768;
-};
-
-export { isMobile };
